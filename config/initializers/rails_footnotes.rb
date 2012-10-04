0aef2a9ce7531cfb70f435d7fd8e521b6b6af392
Tyler Eaves <teaves@reflector.com>
2012-10-04 10:50:25 -0400

    more config tweaks

--- config/initializers/rails_footnotes.rb
+++ config/initializers/rails_footnotes.rb
@@ -1,5 +1,5 @@
 if defined?(Footnotes) && Rails.env.development?
   Footnotes.run! # first of all
-
   # ... other init code
 end
+::NewRelic::Agent.after_fork(:force_reconnect => true) if defined? Unicorn
